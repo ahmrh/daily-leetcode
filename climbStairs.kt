959357d9ecb5379ee4dd1a0f8646e55d32762ae3
ahmrh <ahmadrolandi01@gmail.com>
2023-07-19 09:25:29 +0700

    lifting redundant return on climbStairs

--- climbStairs.kt
+++ climbStairs.kt
@@ -3,14 +3,14 @@ fun climbStairs(n: Int): Int {
 
     //tabulation
     val savedStep = mutableListOf(1, 2)
-    when (n) {
-        1 -> return 1
-        2 -> return 2
+    return when (n) {
+        1 -> 1
+        2 -> 2
         else -> {
             for (i in 2..n) {
                 savedStep.add(savedStep[i - 1] + savedStep[i - 2])
             }
-            return savedStep[n - 1]
+            savedStep[n - 1]
         }
     }
 }
