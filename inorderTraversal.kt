3b2cb55956c20d6d5b446ed01ceb43860f0f38fa
ahmrh <ahmadrolandi01@gmail.com>
2023-08-01 08:36:34 +0700

    94. Binary Tree Inorder Traversal

---
+++ inorderTraversal.kt
@@ -0,0 +1,13 @@
+fun inorderTraversal(root: TreeNode?): List<Int> {
+    val list = mutableListOf<Int>()
+    fun traverse(node: TreeNode?){
+        if(node == null) return
+
+        list.add(node.`val`)
+        traverse(node.left)
+        traverse(node.right)
+
+    }
+    traverse(root)
+    return list
+}
